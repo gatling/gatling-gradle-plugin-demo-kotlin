28c8c3d44c3328fe298bb64091e682c102f76ec8
Simon Scholz <opensource.simon@gmail.com>
2023-01-16 17:01:08 +0100

    chore: make use of kotlin gradle dsl instead of groovy (#4)
    Motivation:
    Since Kotlin is being used for this repository, the usage of the Kotlin DSL might be preferred over the usage of the Groovy DSL.
    
    Modifications:
    * replace build.gradle file to build.gradle.kts
    * put kotlinVersion in gradle.properties
    * add .idea folder in gitignore
    
    Result:
    Remove last groovy code in this repository

---
+++ build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    kotlin("jvm")
+    kotlin("plugin.allopen")
+
+    // The following line allows to load io.gatling.gradle plugin and directly apply it
+    id("io.gatling.gradle") version "3.9.0.2"
+}
+
+gatling {
+    // WARNING: options below only work when logback config file isn't provided
+    logLevel = "WARN" // logback root level
+    logHttp = io.gatling.gradle.LogHttp.NONE // set to 'ALL' for all HTTP traffic in TRACE, 'FAILURES' for failed HTTP traffic in DEBUG
+
+    enterprise.closureOf<Any> {
+        // Enterprise Cloud (https://cloud.gatling.io/) configuration reference: https://gatling.io/docs/gatling/reference/current/extensions/gradle_plugin/#working-with-gatling-enterprise-cloud
+        // Enterprise Self-Hosted configuration reference: https://gatling.io/docs/gatling/reference/current/extensions/gradle_plugin/#working-with-gatling-enterprise-self-hosted
+    }
+}
+
+repositories {
+    mavenCentral()
+}
