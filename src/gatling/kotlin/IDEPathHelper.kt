411b06e0ab27c668eec0a8b86275717e845d6676
slandelle <slandelle@gatling.io>
2024-03-18 22:20:17 +0100

    chore: don't rely on gatling.conf

--- src/gatling/kotlin/IDEPathHelper.kt
+++ src/gatling/kotlin/IDEPathHelper.kt
@@ -1,16 +1,22 @@
+import java.io.File
 import java.nio.file.Path
-import java.nio.file.Paths
-import java.util.Objects.requireNonNull
 
 object IDEPathHelper {
 
-  private val projectRootDir = Paths.get(requireNonNull(javaClass.getResource("gatling.conf"), "Couldn't locate gatling.conf").toURI()).parent.parent.parent.parent
-  private val gradleBuildDirectory = projectRootDir.resolve("build")
+  val gradleBinariesDirectory =
+          System.getProperty("java.class.path").split(File.pathSeparator)
+                  .stream()
+                  .map { cpe ->  Path.of(cpe) }
+                  .filter { cpe -> cpe.endsWith("gatling") }
+                  .findFirst()
+                  .get()
+
+  private val gradleBuildDirectory = gradleBinariesDirectory.parent.parent.parent
+  private val projectRootDir = gradleBuildDirectory.parent
   private val gradleSrcDirectory = projectRootDir.resolve("src").resolve("gatling")
 
   val gradleSourcesDirectory: Path = gradleSrcDirectory.resolve("kotlin")
   val gradleResourcesDirectory: Path = gradleSrcDirectory.resolve("resources")
-  val gradleBinariesDirectory: Path = gradleBuildDirectory.resolve("classes").resolve("kotlin").resolve("gatling")
   val resultsDirectory: Path = gradleBuildDirectory.resolve("reports").resolve("gatling")
   val recorderConfigFile: Path = gradleResourcesDirectory.resolve("recorder.conf")
 }
