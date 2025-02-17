8e86eb90f3a371287caa59210356d6bdc0af6bbe
slandelle <slandelle@gatling.io>
2025-02-17 21:05:10 +0100

    chore: upgrade plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
     kotlin("plugin.allopen") version "2.1.10"
 
     // The following line allows to load io.gatling.gradle plugin and directly apply it
-    id("io.gatling.gradle") version "3.13.4"
+    id("io.gatling.gradle") version "3.13.4.1"
 }
 
 gatling {
