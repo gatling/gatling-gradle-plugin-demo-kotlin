94d1f34fcf9dcbd9d0eaf9a5c14b1d3e0ba141c1
slandelle <slandelle@gatling.io>
2025-05-12 12:22:30 +0200

    feat: upgrade Gatling 3.14.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
     kotlin("plugin.allopen") version "2.1.20"
 
     // The following line allows to load io.gatling.gradle plugin and directly apply it
-    id("io.gatling.gradle") version "3.13.5.4"
+    id("io.gatling.gradle") version "3.14.0"
 }
 
 gatling {
