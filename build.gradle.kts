a25536ab2718e617399d171b1f325fb90e7d3d61
slandelle <slandelle@gatling.io>
2025-12-17 22:44:06 +0100

    chore: upgrade kotlin 2.3.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    kotlin("jvm") version "2.2.21"
-    kotlin("plugin.allopen") version "2.2.21"
+    kotlin("jvm") version "2.3.0"
+    kotlin("plugin.allopen") version "2.3.0"
 
     // The following line allows to load io.gatling.gradle plugin and directly apply it
     id("io.gatling.gradle") version "3.14.9.1"
